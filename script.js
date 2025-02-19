bc391d6c9ed150138d153ad584da4361dd066b79
Barix1122 <f.zmuda2008@gmail.com>
2025-02-19 16:21:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,35 @@
 var map = L.map('map').setView([51.62, 24.30], 6);
 
 // Dodanie warstwy OpenStreetMap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
 
 var tracker1Marker = null; // Zmienna dla Trackera 1
 
 // Funkcja do pobierania danych GPS z pliku data.json
 async function updateTracker1() {
     try {
-        const response = await fetch("const url = "https:raw.githubusercontent.com/Barix1122/TrackerGPS/main/data.json");
+        const response = await fetch("https://raw.githubusercontent.com/Barix1122/TrackerGPS/main/data.json");
+        
+        if (!response.ok) {
+            throw new Error(`Błąd HTTP: ${response.status}`);
+        }
+        
         const data = await response.json();
         
-        if (tracker1Marker) {
-            tracker1Marker.setLatLng([data.lat, data.lon]); // Aktualizacja pozycji
+        if (data.lat && data.lon) {
+            if (tracker1Marker) {
+                tracker1Marker.setLatLng([data.lat, data.lon]); // Aktualizacja pozycji
+            } else {
+                tracker1Marker = L.marker([data.lat, data.lon]).addTo(map)
+                    .bindPopup(`<b>Tracker 1</b><br>Latitude: ${data.lat}<br>Longitude: ${data.lon}`)
+                    .openPopup();
+            }
+            console.log("Tracker 1 zaktualizowany:", data.lat, data.lon);
         } else {
-            tracker1Marker = L.marker([data.lat, data.lon]).addTo(map)
-                .bindPopup(`<b>Tracker 1</b><br>Latitude: ${data.lat}<br>Longitude: ${data.lon}`)
-                .openPopup();
+            console.warn("Nieprawidłowe dane GPS w pliku JSON:", data);
         }
-        console.log("Tracker 1 zaktualizowany:", data.lat, data.lon);
     } catch (error) {
         console.error("Błąd pobierania danych GPS:", error);
     }
